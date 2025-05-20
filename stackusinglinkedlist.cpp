38dd00fb76f628a90d35a3de591cceb762888ec4
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-20 10:49:13 +0700

    membuat class stack

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -14,3 +14,27 @@ public : //akses modifier
     }
 }; 
 
+class stack // membuat class stack
+{
+private : // akses modifier
+    Node *top; // pointer ke node teratas
+
+public : // akses modifier
+    stack() // contructor stack
+    { 
+
+        top = NULL; // inisialisasi top dengan null
+    }
+
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
+
+   
+}
