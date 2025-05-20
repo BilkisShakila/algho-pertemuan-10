7334b170e9e59bfdd9de5a43344c7db5da7d5f6e
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-20 10:46:29 +0700

    membuat class node

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+class Node // membuat node 
+{
+public : //akses modifier
+    int data; //data yang disimpan 
+    Node *next; //pointer ke node yang berikutnya
+    //contructor
+
+    Node() //node contructor
+    {
+        next = NULL; // inisialisasi next dengan null
+    }
+}; 
+
