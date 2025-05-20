803802b61601d0125552c58fa2a881117e50c4e5
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-20 11:22:20 +0700

    menambahkan penanganan pilihan menu

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -121,4 +121,8 @@ int main()
         case 4:
             cout << "Exiting Program. " << endl;
             break;
+        default:
+            cout << "Invalid choice. Try again]." << endl;
+            break;
+
         }
